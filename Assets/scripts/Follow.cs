d4ace4efaa5ad92f73c1a4841e506efe9dd75a40
Anže Pečar <ensmotko@gmail.com>
2015-08-22 13:52:33 +0200

    Implement basic movement (with click to move)

---
+++ Assets/scripts/Follow.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Follow : MonoBehaviour {
+
+	public GameObject mark;
+	private Vector3 pos;
+
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		pos = mark.transform.position;
+		transform.position = new Vector3(pos.x, pos.y, transform.position.z);
+	}
+}
---
+++ Assets/scripts/Movement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+	private Vector3 target;
+
+	// Use this for initialization
+	void Start () {
+		target = transform.position;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (Input.GetMouseButtonDown(0)) {
+			target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			target.z = transform.position.z;
+		}
+		transform.position = Vector3.MoveTowards(transform.position, target, 5 * Time.deltaTime);
+	}
+}
