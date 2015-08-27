3ef28a8605a92e43a8e8110fa1c6683f3de8f644
Anže Pečar <ensmotko@gmail.com>
2015-08-27 23:30:17 +0200

    Add a basic HUD

---
+++ Assets/ToggleButton.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ToggleButton : MonoBehaviour {
+
+	public float timeOut;
+	public string key;
+	public Sprite up;
+	public Sprite down;
+
+	private bool inactive;
+	private Image image;
+
+	// Use this for initialization
+	void Start () {
+		image = GetComponent<Image>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(key) && !inactive) {
+			inactive = true;
+			image.sprite = down;
+			Invoke ("Deactivate", timeOut);
+		}
+	}
+	void Deactivate() {
+		image.sprite = up;
+		inactive = false;
+	}
+}
