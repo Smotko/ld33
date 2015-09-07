37a8eedeb6e9655f9ddf4de62dd5488aa3107043
Anže Pečar <ensmotko@gmail.com>
2015-09-08 00:06:01 +0200

    Add Blood effects and zergy death sprite

---
+++ Assets/SortLayerFix.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SortLayerFix : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<ParticleSystem>().GetComponent<Renderer>().sortingLayerName = "foreground";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Assets/scripts/Health.cs
+++ Assets/scripts/Health.cs
@@ -6,9 +6,11 @@ public class Health : MonoBehaviour {
 	public int health = 100;
 	public bool hasShield = false;
 	public GameObject spriteComponent;
+	public GameObject dead;
 	
 	private Attack attack;
 	private SpriteRenderer sprite;
+	private ParticleSystem blood;
 
 	// Use this for initialization
 	void Start () {
@@ -20,6 +22,8 @@ public class Health : MonoBehaviour {
 		} else {
 			sprite = GetComponent<SpriteRenderer>();
 		}
+		blood = GetComponentInChildren<ParticleSystem>();
+
 	}
 	
 	// Update is called once per frame
@@ -35,12 +39,15 @@ public class Health : MonoBehaviour {
 		}
 		sprite.color = Color.red;
 		Invoke ("ResetColor", 0.1f);
+		blood.Play();
 		if (!GameManager.instance.playerAlive) {
 			amount = 500;
 		}
+
 		health -= amount;
 		if (health <= 0) {
 			GameManager.instance.ObjectDestroyed(gameObject);
+			Instantiate(dead, transform.position, Quaternion.identity);
 		}
 	}
 }
